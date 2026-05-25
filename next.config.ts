26cea65bcecf7ec27a56d9b756002ae544b38b8e
Tiago Satur <tiagosatur@gmail.com>
2026-05-25 17:51:46 -0300

    add Sentry error tracking with Session Replay

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {};
 
-export default withNextIntl(nextConfig);
+export default withSentryConfig(withNextIntl(nextConfig), {
+  org: "nasus-digital",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  disableLogger: true,
+});
